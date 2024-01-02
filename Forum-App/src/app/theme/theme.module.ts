160a4e358df8701686ea600671b3d0b60ecd1bea
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-01-02 12:01:57 +0200

    Welcome Component

--- Forum-App/src/app/theme/theme.module.ts
+++ Forum-App/src/app/theme/theme.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
+import { UserModule } from '../user/user.module';
 
 @NgModule({
   declarations: [NewThemeComponent, CurrentThemeComponent],
-  imports: [CommonModule, ThemeRoutingModule],
+  imports: [CommonModule, ThemeRoutingModule, UserModule],
 })
 export class ThemeModule {}
