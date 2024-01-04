3ef4c46a92fa25bdaa2683a43be06f1175abb1b8
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-01-04 20:27:19 +0200

    Email Validator/ Email Directive

--- Forum-App/src/app/user/login/login.component.ts
+++ Forum-App/src/app/user/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,14 @@ import { Route, Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  emailDomains = EMAIL_DOMAINS;
+  
   constructor(private userService: UserService, private router: Router) {}
 
-  login(email: string, password: string): void {
-    this.userService.login();
-    this.router.navigate(['/']);
+  login(form: NgForm): void {
+    console.log(form.value);
+
+    // this.userService.login();
+    // this.router.navigate(['/']);
   }
 }
