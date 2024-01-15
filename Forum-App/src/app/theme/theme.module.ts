178becc00b286e7cbeaa4a01ce79fcfd9e360c24
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-01-15 11:52:43 +0200

    New Theme Form (Validations - template-driven forms)

--- Forum-App/src/app/theme/theme.module.ts
+++ Forum-App/src/app/theme/theme.module.ts
@@ -4,9 +4,10 @@ import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
 import { UserModule } from '../user/user.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NewThemeComponent, CurrentThemeComponent],
-  imports: [CommonModule, ThemeRoutingModule, UserModule],
+  imports: [CommonModule, ThemeRoutingModule, UserModule, FormsModule],
 })
 export class ThemeModule {}
